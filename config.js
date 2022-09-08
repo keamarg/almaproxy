0a05c8240c93a5f4261f45194c4438417beea587
Martin Gundtoft <marg@kea.dk>
2022-09-08 15:49:38 +0200

    ...

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const dotenv = require("dotenv");
-const { Http2ServerRequest } = require("http2");
 const path = require("path");
 
 dotenv.config({
   path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`),
 });
+console.log(process.env);
 
 module.exports = {
   NODE_ENV: process.env.NODE_ENV,
