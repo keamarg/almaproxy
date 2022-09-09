353f116961fe8edecd93a75a882b8505214909d3
Martin Gundtoft <marg@kea.dk>
2022-09-09 13:08:03 +0200

    ...

--- almaproxy.js
+++ almaproxy.js
@@ -61,7 +61,7 @@ app.use(function (req, res) {
 });
 
 //create node.js http server and listen on port
-http.createServer(app).listen(port);
+https.createServer(app).listen(port);
 
 // console.log(`NODE_ENV=${node_env}`);
 
