74e29049098f34c3eefb271b0b9defcb99a8044d
Martin Gundtoft <marg@kea.dk>
2022-09-26 13:53:58 +0200

    proxy working - clean

--- app.js
+++ app.js
@@ -1,33 +1,3 @@
-// const express = require("express");
-// const app = express();
-
-// const http = require("http");
-// const host = "127.0.0.1";
-// const port = 8081;
-
-// // const server = http.createServer((req, res) => {
-// //   res.statusCode = 200;
-// //   res.setHeader("Content-Type", "text/plain");
-// //   res.end("Hello World! NodeJS \n");
-// // });
-
-// // server.listen(port, hostname, () => {
-// //   console.log(`Server running at http://${hostname}:${port}/`);
-// // });
-
-// app.get("/almaproxy", (req, res, next) => {
-//   res.send("Home folder.");
-// });
-
-// app.get("/almaproxy/info", (req, res, next) => {
-//   res.send("This is a proxy service which proxies to Alma.");
-// });
-
-// // Start the Proxy
-// app.listen(port, host, () => {
-//   console.log(`Starting Proxy at ${host}:${port}`);
-// });
-
 const config = require("./config.js");
 const express = require("express");
 const morgan = require("morgan");
@@ -39,37 +9,15 @@ const url = "https://api-eu.hosted.exlibrisgroup.com";
 const app = express();
 
 // Configuration
-var port = "8081"; //config.PORT;
-var host = "127.0.0.1"; //config.HOST;
-var api_key = "l8xx1d07986de63b4d0289d5bac8374d99c3"; //config.API_KEY;
-
-// var port = config.PORT;
-// var host = config.HOST;
-// var node_env = config.NODE_ENV;
-// var key_path = config.KEY_PATH;
-// var cert_path = config.CERT_PATH;
-// var api_key = config.API_KEY;
+var port = config.PORT;
+var host = config.HOST;
+var api_key = config.API_KEY;
 
 // Logging
 app.use(morgan("dev"));
 
-// app.use(function (req, response, next) {
-//   response.setHeader("Access-Control-Allow-Origin", "*");
-//   response.setHeader("Access-Control-Allow-Credentials", "true");
-//   response.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET,HEAD,OPTIONS,POST,PUT"
-//   );
-//   response.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-//   );
-//   // response.setHeader("Authorization", `apikey ${api_key}`);
-//   next();
-// });
-
 // Info GET endpoint
-app.get("/almaproxy/info", (req, res, next) => {
+app.get("/info", (req, res, next) => {
   res.send("This is a proxy service which proxies to Alma.");
 });
 
@@ -83,110 +31,12 @@ app.use("", (req, res, next) => {
   }
 });
 
-let queryPath = null;
-let queryLimit = null;
-let queryOffSet = null;
-let queryUrl = null;
-
-// Parse request
-app.use("", (req, res, next) => {
-  queryUrl = req.url;
-  queryPath = req.query.path;
-  queryLimit = req.query.limit;
-  queryOffSet = req.query.offset;
-  console.log("request url: " + req.url);
-  next();
-});
-
-// app.use("", (req, res, next) => {
-//   res.send(queryPath);
-//   next();
-// });
-
-// let offSet = 0;
-// const increaseOffSet = (value) => {
-//   console.log("offset: " + offSet);
-//   if ((offSet = 0)) {
-//     offSet = offSet + value;
-//     return 0;
-//   } else {
-//     offSet = offSet + value;
-//     return offSet;
-//   }
-// };
-
-// let first = true;
-// let offSet = 0;
-// let limit = 100;
-// const rewriteFn = function (path, req) {
-//   if (first == true) {
-//     first = false;
-//   } else {
-//     offSet = offSet + 12;
-//   }
-//   return path.replace(
-//     "/almaproxy/productlist",
-//     `/almaws/v1/electronic/e-collections/618551140007387/e-services/628551130007387/portfolios?limit=${limit}` //?limit=${limit}&offset=${offSet}`
-//   );
-// };
-
-// Proxy endpoints
-// app.use(
-//   "/almaproxy/productlist",
-//   createProxyMiddleware({
-//     target: url,
-//     changeOrigin: true,
-//     // pathRewrite: {
-//     //   [`^/productlist`]: `/almaws/v1/electronic/e-collections/618551140007387/e-services/628551130007387/portfolios?limit=12&offset=${increaseOffSet(
-//     //     12
-//     //   )}`,
-//     // },
-//     pathRewrite: rewriteFn,
-//   })
-// );
-
-// app.use(
-//   "/almaproxy/product/:id",
-//   createProxyMiddleware({
-//     target: url,
-//     changeOrigin: true,
-//     pathRewrite: {
-//       [`^/almaproxy/product`]: "/almaws/v1/bibs/",
-//     },
-//   })
-// );
-
-const rewriteFn = function (path, req) {
-  // console.log("min path:" + path);
-  return path.replace(
-    "/almaproxy", // `${queryPath}` // `/${queryPath}&limit=${queryLimit}&offset=${queryOffSet}` //?limit=${limit}&offset=${offSet}`
-    // "almaws/v1/electronic/e-collections/618551140007387/e-services/628551130007387/portfolios"
-    `/`
-    // "almaws/v1/electronic/e-collections/618551140007387/e-services/628551130007387/portfolios"
-  );
-};
-
 const options = {
   target: url,
   changeOrigin: true,
-  // pathRewrite: rewriteFn,
-  // pathRewrite: {
-  //   [`^/almaproxy`]: `almaws/v1/electronic/e-collections/618551140007387/e-services/628551130007387/portfolios`,
-  // },
 };
 
-app.use(
-  "/",
-  createProxyMiddleware(
-    options
-    // target: url,
-    // changeOrigin: true,
-    // pathRewrite: rewriteFn,
-    // pathRewrite: {
-    //   [`^/almaproxy`]: `almaws/v1/electronic/e-collections/618551140007387/e-services/628551130007387/portfolios&limit=22`,
-    // },
-  )
-);
+app.use("/", createProxyMiddleware(options));
 
 // Start the Proxy
 app.listen(port, host, () => {
