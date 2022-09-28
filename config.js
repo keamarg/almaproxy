32e76a888f3fd974d859c5e81fd31402e54c1f86
Martin Gundtoft <marg@kea.dk>
2022-09-28 13:50:55 +0200

    proxy testing

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 const path = require("path");
 
 dotenv.config({
-  path: path.resolve(__dirname, `production.env`), //path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`),
+  path: path.resolve(__dirname, `.production.env`), //path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`),
 });
 
 process.env.NODE_ENV = "production";
